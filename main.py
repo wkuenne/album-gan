d27420fb85cfd6dde04f2aefa845282e3c9bfbb3
Mateo Encarnacion <encar7435@gapps.uwcsea.edu.sg>
2019-12-03 20:10:11 -0500

    added main file, gitignore, main, and requirements.txt

---
+++ main.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, BatchNormalization, LeakyReLU, Reshape, Conv2DTranspose
+
+
+import numpy as np
+
+import os
+import argparse
+
+def main():
+    # Load a batch of images (to feed to the discriminator)
+    print('hello world!')
+
+if __name__ == '__main__':
+   main()
+
+
